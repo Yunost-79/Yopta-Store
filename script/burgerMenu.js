590b992747e1f6bc01978bee7e466a5c130dd8d3
vskorobohatov <vskorobohatov@geeksters.io>
2022-09-24 21:41:43 +0300

    Backdrop fix

--- script/burgerMenu.js
+++ script/burgerMenu.js
@@ -9,12 +9,19 @@ const toggleBurgerMenu = () => {
 
   if (burgerNav.classList.contains("active")) {
     burgerBackdrop.classList.remove("active");
+    setTimeout(() => {
+      burgerBackdrop.classList.remove('shown');
+    }, 700);
+
     burgerNav.classList.remove('active');
     setTimeout(() => {
       burgerNav.classList.remove('shown');
     }, 700);
   } else {
-    burgerBackdrop.classList.add("active");
+    burgerBackdrop.classList.add("shown");
+    setTimeout(() => {
+      burgerBackdrop.classList.add("active");
+    });
     burgerNav.classList.add('shown');
     burgerNav.classList.add('active');
   }
